1509ba6ce5506676d1414b8172bd1463d97586ba
Yulius Tjahjadi <yulius.tjahjadi@take2games.com>
2022-10-31 14:25:10 -0700

    parse iteration properly

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ function sleep(t) {
 }
 
 async function main() {
+
+  const argv = yargs.argv
+
+  let total = 100;
+  if (argv._.length === 1) {
+    total = parseInt(argv._[0]) || 100;
+  }
+
   const url = 'wss://social-service-develop.d2dragon.net/mqtt';
   const ws = new WebSocket(url, 'mqtt', {});
   const spinner = ora();
@@ -30,8 +38,6 @@ async function main() {
   
   ws.on('open', async () => {
     spinner.text = `Connected`;
-    const total = 10;
-    // const total = 100000000;
     for (let i = 0; i < total; i++) {
       spinner.text = `sending ${chalk.yellow(i + 1)}/${chalk.yellow(total)} size ${chalk.cyan(buffer.length)} to ${chalk.cyan(url)}`;
       ws.send(buffer);
